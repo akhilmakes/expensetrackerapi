6ee2819afc7654b2995a38e8cd4098c80cf5fccd
Akhil <akhil.makes@gmail.com>
2023-04-11 08:15:24 +0100

    Create Transaction and FindById

--- expense-tracker/src/main/java/com/restapi/expensetracker/repositories/TransactionRepositoryImpl.java
+++ expense-tracker/src/main/java/com/restapi/expensetracker/repositories/TransactionRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.restapi.expensetracker.exceptions.EtBadRequestException;
 import com.restapi.expensetracker.exceptions.EtResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -22,11 +23,11 @@ public class TransactionRepositoryImpl implements TransactionRepository{
             " FROM ET_TRANSACTIONS WHERE USER_ID = ?";
 
     private static final String SQL_FIND_BY_ID = "SELECT TRANSACTION_ID, CATEGORY_ID, USER_ID, AMOUNT, NOTE, TRANSACTION_DATE " +
-            "FROM ET_TRANSACTIONS WHERE TRANSACTION_ID = ? AND CATEGORY_ID ? AND USER_ID = ?";
+            "FROM ET_TRANSACTIONS WHERE USER_ID = ? AND CATEGORY_ID = ? AND TRANSACTION_ID = ?";
 
     private static final String SQL_CREATE = "INSERT INTO ET_TRANSACTIONS (TRANSACTION_ID, CATEGORY_ID, USER_ID, AMOUNT, " +
             "NOTE, TRANSACTION_DATE) " +
-            "VALUES(NEXT_VAL('ET_TRANSACTIONS_SEQ'), ?, ?, ?, ?, ?)";
+            "VALUES(NEXTVAL('ET_TRANSACTIONS_SEQ'), ?, ?, ?, ?, ?)";
 
 
     @Override
@@ -36,7 +37,12 @@ public class TransactionRepositoryImpl implements TransactionRepository{
 
     @Override
     public Transaction findById(Integer userId, Integer categoryId, Integer transactionId) throws EtResourceNotFoundException {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SQL_FIND_BY_ID, new Object[]{userId, categoryId, transactionId},
+                    transactionRowMapper);
+        } catch(Exception e){
+            throw new EtResourceNotFoundException("Transaction Not Found!");
+        }
     }
 
     @Override
@@ -70,4 +76,14 @@ public class TransactionRepositoryImpl implements TransactionRepository{
     public void removeById(Integer userId, Integer categoryId, Integer transactionId) throws EtResourceNotFoundException {
 
     }
+
+    private RowMapper<Transaction> transactionRowMapper = ((rs, rowNum) -> {
+        return new Transaction(rs.getInt("TRANSACTION_ID"),
+                rs.getInt("CATEGORY_ID"),
+                rs.getInt("USER_ID"),
+                rs.getDouble("AMOUNT"),
+                rs.getString("NOTE"),
+                rs.getLong("TRANSACTION_DATE"));
+
+    });
 }
